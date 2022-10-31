1a392a2456f1569f0a7e747744903fcfae580eb4
genius21513 <genius21513@gmail.com>
2022-10-31 09:44:14 +0900

    Add some

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -10,4 +10,35 @@ const provider = new ethers.providers.AlchemyProvider('goerli', API_KEY);
 
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 
-console.log(JSON.stringify(contract.abi));
+// console.log(JSON.stringify(contract.abi));
+
+// Create a signer
+const privateKey = process.env.PRIVATE_KEY;
+const signer = new ethers.Wallet(privateKey, provider);
+
+// Get contract ABI and address
+const abi = contract.abi;
+const contractAddress = '0x45aCff81A13330F2813aD56c598B956F195886af';
+
+// Create a contract instance
+const myNftContract = new ethers.Contract(contractAddress, abi, signer);
+
+// Get the NFT Metadata IPFS URL
+const tokenUri = "https://gateway.pinata.cloud/ipfs/QmUgWZUhn8EgUjmEozauX53c3GwenYMCwAnYqPkRxHR7LA";
+
+// Call mintNFT function
+const mintNFT = async () => {
+    let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri);
+    await nftTxn.wait();
+    console.log(`NFT Minted! Check it out at : https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+}
+
+
+mintNFT()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
+
+
